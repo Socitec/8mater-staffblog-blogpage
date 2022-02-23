bfc74edb1520b71960a0c888a995c753a51f2f81
Yuki <ghost.in.the.shell.innocence9@gmail.com>
2022-02-23 13:32:50 +0900

    add image, margin-top

--- script/script.js
+++ script/script.js
@@ -1,4 +1,23 @@
 $(function(){
 
 
+
+
+
+  // swiper.js
+  const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4
+  });
+  
+  const swiper2 = new Swiper(".mySwiper2", {
+    thumbs: {
+      swiper: swiper
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+
 });
